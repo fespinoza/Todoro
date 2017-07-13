3cf05e3130978dc1555f2ed124b3af4422e92517
Felipe Espinoza <fespinozacast@gmail.com>
2017-07-13 13:57:50 +0200

    Fix indentation and change reference to `Event` for `Task`.

--- Todoro/DetailViewController.swift
+++ Todoro/DetailViewController.swift
@@ -9,16 +9,14 @@
 import UIKit
 
 class DetailViewController: UIViewController {
-
   @IBOutlet weak var detailDescriptionLabel: UILabel!
-
 
   func configureView() {
     // Update the user interface for the detail item.
     if let detail = detailItem {
-        if let label = detailDescriptionLabel {
-            label.text = detail.timestamp!.description
-        }
+      if let label = detailDescriptionLabel {
+        label.text = detail.title
+      }
     }
   }
 
@@ -33,13 +31,10 @@ class DetailViewController: UIViewController {
     // Dispose of any resources that can be recreated.
   }
 
-  var detailItem: Event? {
+  var detailItem: Task? {
     didSet {
-        // Update the view.
-        configureView()
+      // Update the view.
+      configureView()
     }
   }
-
-
 }
-
